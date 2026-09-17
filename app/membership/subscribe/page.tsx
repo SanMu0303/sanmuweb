2e72e413a8d8b3d5864897cf5561d29d49eaf78a
SanMu0303 <jingzhe0325@gmail.com>
2026-09-17 11:03:47 +0800

    Add dedicated membership subscription page

---
+++ app/membership/subscribe/page.tsx
@@ -0,0 +1,4 @@
+import MembershipCheckout from '@/components/MembershipCheckout';
+
+export const metadata={title:'开通会员'};
+export default function MembershipSubscribePage(){return <MembershipCheckout/>}
